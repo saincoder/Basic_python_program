cac002515740e37d15bfbc88d6756ce72643b726
shahidmetex03 <shahid03.metex@gmail.com>
2024-01-19 10:58:43 +0500

    Addition

---
+++ 2_program.py
@@ -0,0 +1,6 @@
+# Write a Python program to do arithmetical operations addition and division.
+# Addition
+num1 = float(input("Enter the first number for addition: "))
+num2 = float(input("Enter the second number for addition: "))
+sum_result = num1 + num2
+print(f"sum: {num1} + {num2} = {sum_result}")
