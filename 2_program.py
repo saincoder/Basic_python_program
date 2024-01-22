e8365e6ccd7edbf796672c3119f956459b1d2b6b
shahidmetex03 <shahid03.metex@gmail.com>
2024-01-22 20:18:31 +0500

    2_program

--- 2_program.py
+++ 2_program.py
@@ -4,3 +4,12 @@ num1 = float(input("Enter the first number for addition: "))
 num2 = float(input("Enter the second number for addition: "))
 sum_result = num1 + num2
 print(f"sum: {num1} + {num2} = {sum_result}")
+
+# Division
+num3 = float(input("Enter the first number for addition: "))
+num4 = float(input("Enter the second number for addition: "))
+if num4 == 0:
+    print("Error: Division by zero is not allowed.")
+else:
+    div_result = num3 / num4
+    print(f"Division: {num3} / {num4} = {div_result}")
